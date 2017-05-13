4dc9839a620daabda3a585aa4e41123b03524665
zdy <dadayy1@outlook.com>
2017-05-13 22:32:49 +0800

    配置 新闻内容 页面

--- code/web/uews/views.py
+++ code/web/uews/views.py
@@ -2,5 +2,26 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import UewsData
 
 # Create your views here.
+def UewContent(request):
+    uews =  UewsData.objects.filter(id=1)
+    for u in uews:
+        title = u.title
+        title_original = u.title_original
+        site_original = u.site_original
+        newstime = u.newstime
+        url_original = u.url_original
+        content_html = u.content_html
+
+
+    return render(request, 'new_content.html', {
+        'title': title,
+        'title_original':title_original,
+        'site_original':site_original,
+        'newstime':newstime,
+        'url_original':url_original,
+        'content_html': content_html,
+
+    })
--- code/web/web/urls.py
+++ code/web/web/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from uews.views import UewContent
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',UewContent)
 ]
